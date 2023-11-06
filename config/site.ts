23954e940c8b6fbfe6d91508212d96fb565f3543
jamumar <umar2491812@gmail.com>
2023-11-06 10:25:40 +0500

    Deployment

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "The Leather Sense",
+  description: "Handmade Leather crafts",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
